c90bd53d64e1a452b3b85fc89423a0d9919dbdca
nawb <nabeel@zaeem.org>
2015-04-06 17:54:46 -0700

    Saved before changing to set data structure

--- count.py
+++ count.py
@@ -1,8 +1,10 @@
 #!/usr/bin python
+import sys
 import oauth2
 import urlparse
 import pytumblr
 import json
+import time
 from unicodedata import normalize
 from pprint import pprint
 
@@ -20,20 +22,45 @@ client = pytumblr.TumblrRestClient(
   'BCADjechYrTsfZFK154mOohXkMP2kOpE0WW8gDIxlueU3viLzC'
 )
 
-followers = []
+sys.stdout.write("Retrieving followers")
 
-for offset in range(0,582,20):
+followers = []
+counter = 0
+offset = 0
+doneCounting = False
+start_time = time.time()
+
+while True:
     response = client.followers('nasedbab.tumblr.com', offset=offset)['users']
+    offset = offset + 20
+    sys.stdout.flush() #to clear output buffer
+    sys.stdout.write(".")
+
     for i in range(0,20):
         try:
-            user = response[i]['name'].encode('ascii', 'ignore') #extract name field and convert from unicode
+            #extract name field and convert from unicode
+            user = response[i]['name'].encode('ascii', 'ignore')
             followers.append(user)
+            counter = counter + 1
         except IndexError:
+            doneCounting = True
             break
-    print "ok"
-        
+    if doneCounting:
+        sys.stdout.write("\n")
+        break
+
+end_time = time.time()
+elapsed_time = end_time - start_time
+print (str(elapsed_time) + " seconds to retrieve " + str(counter) + " followers")
 
 #print followers
+
+# LOAD OLD FOLLOWERS
+oldfollowers = []
+fo = open('old', 'r')
+for line in fo:
+    oldfollowers.append(line.strip())
+fo.close()
 
 fh = open('out', 'w')
 for user in followers:
